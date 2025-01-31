a2608a8f6e148eae95b29c93f203a236904bc666
rohit-codes7 <rohitkr.official7@gmail.com>
2025-01-31 23:04:04 +0530

    commit

--- src/Array/Longest_Substring_Without_Repeating.java
+++ src/Array/Longest_Substring_Without_Repeating.java
@@ -1,10 +1,32 @@
 package Array;
 
+import java.util.ArrayList;
+
 public class Longest_Substring_Without_Repeating {
     public static void main(String[] args) {
+        String s ="abcabccbb";
+        System.out.println(longestSubstring(s));
 
     }
-    public String longestSubstring(int[]char){
+    public static int longestSubstring(String s){
+        int start=0, end=0, maxCount=0;
+
+        ArrayList<Character> list = new ArrayList<>();
+        while(end<s.length()){
+            if(!list.contains(s.charAt(end))){
+                list.add(s.charAt(end));
+                end++;
+                maxCount = Math.max(maxCount,list.size());
+
+            }
+            else{
+                list.remove(0);
+                start++;
+            }
+
+        }
+        return maxCount;
+
 
     }
 }
