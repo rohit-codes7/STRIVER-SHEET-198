9d6ed357e67a82d444fd7178cefd237cf6487d8a
rohit-codes7 <rohitkr.official7@gmail.com>
2025-02-03 23:32:54 +0530

    commit

--- src/Array/Majority_Element.java
+++ src/Array/Majority_Element.java
@@ -1,27 +1,31 @@
 package Array;
 
+
 public class Majority_Element {
     public static void main(String[] args) {
-        int [] nums ={3,2,3};
+        int[] nums = {3, 2, 3};
         System.out.println(majorityElement(nums));
 
     }
-    public  static  int majorityElement(int[] nums){
+
+    public static int majorityElement(int[] nums) {
         int majority = nums[0];
         int votes = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (votes==0){
-                votes=1;
+            if (votes == 0) {
+                votes = 1;
                 majority = nums[i];
-            } else if (majority==nums[i]) {
+            } else if (majority == nums[i]) {
                 votes++;
 
-            }else{
+            } else {
                 // if the next element is different
                 votes--;
+
             }
         }
         return majority;
 
     }
 }
+
---
+++ src/DEMO/Middle_LL.java
@@ -0,0 +1,57 @@
+package DEMO;
+
+public class Middle_LL {
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        // Find the middle node
+        Node middleNode = MiddleLL(head);
+
+        // Display the value of the middle node
+        System.out.println("The middle node value is: " + middleNode.data);
+
+    }
+
+    static Node MiddleLL(Node head){
+        if (head==null || head.next==null) return head;
+        Node fast = head;
+        Node slow = head;
+
+        while(fast.next!=null && fast!=null){
+            fast = fast.next.next;
+            slow= slow.next;
+        }
+        return slow;
+    }
+
+    static Node findMiddleOfLL(Node head) {                 // T.C -> O(N+N/2)  S.C -> 0(1)
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // step 1 : Count the nodes
+        Node temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        temp = head;
+        int middle = (count / 2) + 1;
+        while (temp != null) {
+            middle = middle - 1;
+
+            if (middle == 0) {
+                break;
+            }
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
