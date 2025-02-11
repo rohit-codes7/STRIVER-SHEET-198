21fcb83a3d56180472b0b8048131f6f292aa2b3e
rohit-codes7 <rohitkr.official7@gmail.com>
2025-02-11 22:32:48 +0530

    commit

---
+++ src/Random/Clear_Digits.java
@@ -0,0 +1,15 @@
+package Random;
+
+public class Clear_Digits {
+
+    public static void main(String[] args) {
+        String s = "abc1";
+        System.out.println(clearDigits(s));
+
+
+   }
+    public static String clearDigits(String s){
+
+        return s;
+    }
+}
