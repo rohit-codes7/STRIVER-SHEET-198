bebf860eb12a2363d1adbfd053534c758c9a78f1
rohit-codes7 <rohitkr.official7@gmail.com>
2025-02-18 10:56:07 +0530

    commit

--- src/Recursion/Subset.java
+++ src/Recursion/Subset.java
@@ -3,26 +3,47 @@ package Recursion;
 import java.util.*;
 
 public class Subset {
+    public static List<List<Integer>> result = new ArrayList<>();
     public static void main(String[] args) {
         int nums[] = {1, 2, 3};
         System.out.println(subsets(nums));
 
     }
 
+//    public static List<List<Integer>> subsets(int[] nums) {
+//        List<List<Integer>> resultList = new ArrayList<>();
+//        backtrack(resultList, new ArrayList<>(), nums, 0);
+//        return resultList;
+//    }
+//
+//    private static void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int[] nums, int start) {
+//        resultSets.add(new ArrayList<>(tempSet));
+//        for (int i = start; i < nums.length; i++) {
+//            tempSet.add(nums[i]);
+//
+//            backtrack(resultSets, tempSet, nums, i + 1);
+//            tempSet.remove(tempSet.size() - 1);
+//
+//        }
+//    }
     public static List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        backtrack(resultList, new ArrayList<>(), nums, 0);
-        return resultList;
+        List<Integer> temp = new ArrayList<>();
+        solve(nums,0,temp);
+        return result;
+
     }
-
-    private static void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int[] nums, int start) {
-        resultSets.add(new ArrayList<>(tempSet));
-        for (int i = start; i < nums.length; i++) {
-            tempSet.add(nums[i]);
-
-            backtrack(resultSets, tempSet, nums, i + 1);
-            tempSet.remove(tempSet.size() - 1);
+    private static void solve(int[]nums,int idx, List<Integer> temp){
+        if(idx>= nums.length){
+            result.add(new ArrayList<>(temp));
+            return;
 
         }
+        temp.add(nums[idx]);
+        solve(nums,idx+1,temp);
+        temp.remove(temp.size()-1);
+        solve(nums,idx+1,temp);
     }
+
 }
+
+
--- src/Recursion/Subset_II.java
+++ src/Recursion/Subset_II.java
@@ -1,20 +1,35 @@
 package Recursion;
 import java.util.*;
 public class Subset_II {
+   static List<List<Integer>> result = new ArrayList<>();
     public static void main(String[] args) {
+        int [] nums ={1,2,2};
+        System.out.println(subsetsWithDup(nums));
 
     }
-    public List<List<Integer>> subsetsWithDup(int[] nums){
 
-        return subsetsWithDup(nums);
+    public static   List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        Arrays.sort(nums);
+        List<Integer> temp = new ArrayList<>();
+        backtrack(nums,0,temp);
+        return result;
+
+
     }
-    static void subsetHelper(int idx,ArrayList<Integer> arr,int sub, int N,ArrayList<Integer> subset){
+    private  static void backtrack (int [] nums, int idx, List<Integer> temp ){
+        result.add(new ArrayList<>(temp));
+        for(int i = idx ; i<nums.length;i++){
+            if(i>idx && nums[i]== nums[i-1]) continue;
 
-        if (idx==N){
-            subset.add(sub);
-            return;
+
+            temp.add(nums[i]);
+            backtrack(nums,i+1,temp);
+            temp.remove(temp.size()-1);
+
         }
-        //pick the element
-
     }
+
+
+
 }
